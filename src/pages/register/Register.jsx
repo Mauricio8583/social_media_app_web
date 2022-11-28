9f359dbb3e84ad813b4391c0a96fdd740639146a
Mauricio Oliveira <mauricio.oliveira8583@gmail.com>
2022-11-28 17:50:11 -0300

    Register page layout

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,23 +1,25 @@
-import './login.scss'
+import './register.scss'
 
-export const Login = () => {
+export const Register = () => {
   return (
-    <div className='login'>
+    <div className='register'>
         <div className='card'>
             <div className='left'>
                 <h1>Bem Vindo</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                  Possimus rem labore praesentium fugiat voluptas? Ducimus, excepturi accusantium saepe, 
                 delectus incidunt commodi obcaecati quae quam perferendis esse debitis, optio aspernatur rem.</p>
-                <span>Ainda não tem uma conta?</span>
-                <button>Cadastre-se</button>
+                <span>Já tem uma conta?</span>
+                <button>Login</button>
             </div>
             <div className='right'>
-                <h1>Login</h1>
+                <h1>Cadastro</h1>
                 <form>
                     <input type="text" placeholder='Username' />
+                    <input type="email" placeholder='E-mail' />
                     <input type="password" placeholder='Senha' />
-                    <button>Login</button>
+                    <input type="text" placeholder='Nome' />
+                    <button>Cadastrar</button>
                 </form>
             </div>
         </div>
