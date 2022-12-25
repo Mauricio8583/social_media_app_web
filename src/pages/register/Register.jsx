5e24c63236f285cb9f8f7aa441a2dd095b35dee8
Mauricio Oliveira <mauricio.oliveira8583@gmail.com>
2022-12-25 18:44:57 -0300

    Making axios requisition to register new user

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,32 @@
 import { Link } from 'react-router-dom'
 import './register.scss'
+import { useState } from 'react'
+import axios from 'axios'
 
 export const Register = () => {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [err, setErr] = useState(null);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try{
+        await axios.post("http://localhost:4000/api/auth/register", {
+            username,
+            email,
+            password,
+            name
+        } );
+
+    }catch(err){
+        setErr(err);
+    }
+  }
+
   return (
     <div className='register'>
         <div className='card'>
@@ -16,11 +41,12 @@ export const Register = () => {
             <div className='right'>
                 <h1>Cadastro</h1>
                 <form>
-                    <input type="text" placeholder='Username' />
-                    <input type="email" placeholder='E-mail' />
-                    <input type="password" placeholder='Senha' />
-                    <input type="text" placeholder='Nome' />
-                    <button>Cadastrar</button>
+                    <input type="text" placeholder='Username' name='username' onChange={(e) => setUsername(e.target.value)} />
+                    <input type="email" placeholder='E-mail' name='email' onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder='Senha' name='pass' onChange={(e) => setPassword(e.target.value)} />
+                    <input type="text" placeholder='Nome' name='name' onChange={(e) => setName(e.target.value)} />
+                    {err && err}
+                    <button onClick={handleClick}>Cadastrar</button>
                 </form>
             </div>
         </div>
